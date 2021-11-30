07effbdee80227005e265e89bf1af0150b35e687
John Neijzen <john.neijzen@live.com>
2021-11-30 09:03:19 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello Welcome to calculator");
+    println!("Enter your first number");
+    let mut first_number = String::new();
+    io::stdin()
+        .read_line(&mut first_number)
+        .expect("Failed to read line");
+
+    println!("Enter your Second number");
+    let mut second_number = String::new();
+    io::stdin()
+        .read_line(&mut second_number)
+        .expect("Failed to read line");
+
+    println!("first number {} and second number {}", first_number, second_number);
+
+    println!("Enter Add, Sub, Div, or Mul");
+    let mut choice = String::new();
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
+
 }
